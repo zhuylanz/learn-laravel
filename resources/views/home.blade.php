b614fcbdff667875ffe54bbee71f2da269a2daf5
Lan Nguyen <zhuylanz@gmail.com>
2020-03-23 22:58:43 +0700

    init laravel sample with templating

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/main')
+
+@section('body')
+  <!-- Header -->
+  @include('partials/home--banner')
+
+  <!-- Services -->
+  @include('partials/home--services')
+@endsection
---
+++ resources/views/second-page.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/main')
+
+@section('body')
+  <!-- Header -->
+  @include('partials/second-page--about')
+
+  <!-- Services -->
+  @include('partials/second-page--portfolio')
+@endsection
